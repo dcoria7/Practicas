0c77f5fc22b2eee3e9f96bd3c759467a9593cf68
D C <dc@MacBook-Pro-de-D.local>
2014-03-06 19:38:54 -0600

    Initial Commit

---
+++ Animation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Animation
+//
+//  Created by D C on 3/6/14.
+//  Copyright (c) 2014 D C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Animation/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Animation
+//
+//  Created by D C on 3/6/14.
+//  Copyright (c) 2014 D C. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
